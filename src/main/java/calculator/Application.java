bb2d48130c2651288fa9a104a5027fa0c51b69c9
Wooseok Jang <wowseok2774@gmail.com>
2024-10-15 19:15:37 +0900

    feat: Application 클래스 생성 및 InputHandler, Calculator 클래스 호출 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,11 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        InputHandler inputHandler = new InputHandler();
+        Calculator cal = new Calculator(inputHandler.getUserInput());
+        cal.extractNumbers();
+
+
     }
 }
