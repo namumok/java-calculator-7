51bc2204d2f7932ed7d245be10a67866c1b03768
Wooseok Jang <wowseok2774@gmail.com>
2024-10-16 16:42:09 +0900

    feat:(Application): 프로그램의 실행 흐름 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,10 +4,20 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        OutputHandler outputHandler = new OutputHandler();
+        outputHandler.guideComment();
+
         InputHandler inputHandler = new InputHandler();
         Calculator cal = new Calculator(inputHandler.getUserInput());
-        cal.extractNumbers();
 
+        String delimiter = cal.extractDelimiter();
+        String[] numbers = cal.extractNumbers(delimiter);
+
+        int sum = cal.sumStringArray(numbers);
+
+        //outputHandler.printExtractedValues(delimiter, numbers);
+
+        outputHandler.printSum(sum);
 
     }
 }
